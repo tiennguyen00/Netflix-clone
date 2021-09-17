719524418712c5f853780f5c06fb29566045c767
18521487@gm.uit.edu.vn <18521487@gm.uit.edu.vn>
2021-09-17 23:29:54 +0700

    UI basic (OptForm)

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Route, Switch } from 'react-router-dom';
 import { FaqsContainer } from './containers/faqs';
 import { FooterContainer } from './containers/footer';
 import JumbotronContainer from './containers/jumbotron';
+import * as ROUTES from './constants/routes';
 
 function App() {
   return (
-    <>
+    <Route>
       <JumbotronContainer />
       <FaqsContainer />
       <FooterContainer />
-    </>
+    </Route>
   );
 }
 

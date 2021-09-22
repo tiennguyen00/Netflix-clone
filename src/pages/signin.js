c9073c7e9f3884c298b999e69180c6c3785243b3
18521487@gm.uit.edu.vn <18521487@gm.uit.edu.vn>
2021-09-22 22:51:44 +0700

    Basic UI (Sign In)

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,5 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FooterContainer } from '../containers/footer';
+import HeaderContainer from '../containers/header';
+import { Form } from '../components';
+import { enableMultiTabIndexedDbPersistence } from '@firebase/firestore';
 
 export default function Signip() {
-  return <div>Hello from signip</div>;
+  const [emailAddress, setEmailAddress] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const isInValid = password === '' || emailAddress === '';
+
+  const handleSignIn = (event) => {
+    event.preventDefault();
+    // Firebase work here
+  };
+
+  return (
+    <>
+      <HeaderContainer>
+        <Form>
+          <Form.Title>Sign In</Form.Title>
+          {error && <Form.Error>{error}</Form.Error>}
+          <Form.Base onSubmit={handleSignIn} mothod="POST">
+            <Form.Input
+              placeholder="Email address"
+              value={emailAddress}
+              onChange={({ target }) => setEmailAddress(target.value)}
+            />
+            <Form.Input
+              placeholder="Password"
+              type="password"
+              autoComplete="off"
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            <Form.Submit disabled={isInValid} type="submit">
+              Sign In
+            </Form.Submit>
+          </Form.Base>
+          <Form.Text>
+            New to Netflix? <Form.Link to="./signup">Sign up now</Form.Link>
+          </Form.Text>
+          <Form.TextSmall>
+            {' '}
+            This page is protected by Google CAPTCHA to ensure you are not a
+            robot. Learn more
+          </Form.TextSmall>
+        </Form>
+      </HeaderContainer>
+      <FooterContainer />
+    </>
+  );
 }
